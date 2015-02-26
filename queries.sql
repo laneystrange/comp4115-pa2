ef28a6fcd0d94ff24f4428551ce7304972ef6fb3
apatrick0516 <apatrick0516@gmail.com>
2015-02-26 13:19:57 -0600

    PA2.2

--- queries.sql
+++ queries.sql
@@ -9,3 +9,5 @@ select sum(prize) as prizeSum from tournaments;
 select count(*) from teams where disbanded is null;
 
 select name from players where id in (select playerID from teamMembers where teamID in (select teamID from teams where disbanded is not null));
+
+select * from matches where (Awins = 2 and Bwins = 3) OR (Bwins = 2 and Awins - 3);
