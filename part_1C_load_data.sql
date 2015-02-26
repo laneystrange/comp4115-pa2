730c8d637cedd58a342d9ca4b080c36e4879b63a
Eric Goddard <egoddard1010@gmail.com>
2015-02-26 11:17:32 -0600

    Part 1C - SQL expressions to load the csv files into the shadowkeep database.

---
+++ part_1C_load_data.sql
@@ -0,0 +1,40 @@
+/* Lets be inclusive. Genders from Facebook.*/
+LOAD DATA LOCAL INFILE 'part_1C_data/genders.csv' INTO TABLE genders
+    LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE 'part_1C_data/players.csv' INTO TABLE players
+    FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' 
+    (first_name, last_name, gender, handle, dob, country);
+
+LOAD DATA LOCAL INFILE 'part_1C_data/teams.csv' INTO TABLE teams
+    FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n'
+    (team_name, date_formed, date_disbanded);
+
+LOAD DATA LOCAL INFILE 'part_1C_data/team_players.csv' INTO TABLE team_players
+    FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n'
+    (team_id, player_id);
+
+
+LOAD DATA LOCAL INFILE 'part_1C_data/events.csv' INTO TABLE events
+    FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n'
+    (event_name, event_city, event_state, start_date, end_date, grand_prize, event_winner);
+
+LOAD DATA LOCAL INFILE 'part_1C_data/event_teams.csv' INTO TABLE event_teams
+    FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n'
+    (event_id, team_id);
+
+/*LOAD DATA LOCAL INFILE 'part_1C_data/matches_rounds.csv' INTO TABLE matches
+    FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+    (event_id, team_one, team_two, team_one_wins, team_two_wins); */
+LOAD DATA LOCAL INFILE 'part_1C_data/matches.csv' INTO TABLE matches
+    FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+    (event_id, winning_team);
+
+LOAD DATA LOCAL INFILE 'part_1C_data/match_results.csv' INTO TABLE match_results
+    FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+    (match_id, team_id, wins);
+
+/*LOAD DATA LOCAL INFILE 'part_1C_data/rounds.csv' INTO TABLE rounds
+    FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+    (match_id, round_number, victor);
+*/
