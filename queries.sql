fde391a43c058df5c20ce81babdd87d5ef9775af
Mars <mariavyl_10491@yahoo.com>
2015-02-26 12:01:59 -0600

    Queries done

--- queries.sql
+++ queries.sql
@@ -2,58 +2,31 @@ select realName, dob from players where gender = 'female';
 
 select realName, handle from players where country = 'Australia';
 
-select * from matches where (team1_score = 0 AND team2_score = 3) OR (team1_score = 3 AND team2_score = 0);
+select * from matches 
+where (team1_score = 0 AND team2_score = 3) 
+OR (team1_score = 3 AND team2_score = 0);
 
 select sum(prize) from tournaments;
 
 select count(*) from teams where activity = 'active';
 
-select distinct(realName) from players  join members on memberName = handle join teams on teams.teamId = members.teamId where teams.activity = 'not active';
+select distinct(realName) 
+from players join members on memberName = handle 
+join teams on teams.teamId = members.teamId 
+where teams.activity = 'not active';
 
+select winner from tournaments where prize = 
+(select max(prize) from tournaments where winner is not null);
 
+select winner from matches group by winner order by count(*) desc limit 1;
 
+select winner from tournaments where winner in 
+(select teamName from teams where teamId in 
+(select teamId from members where memberName = "GingerRoots75")) 
+UNION select sum(prize) from tournaments where winner in 
+(select teamName from teams where teamId in 
+(select teamId from members where memberName = "GingerRoots75"));
 
-
-
-
-
-
-
-
-
-
-* Which team has been the most successful (won the most money)?
-
-* Which team has been the winningest (won the most matches)?
-
-* How much money has player [PlayerName] won at tournaments, and playing for which teams?
-
-* Another query of your choosing. Ask something interesting about your data.
-
-
-
-
-
-
-
-
- List the real name and birthdate of every female player.
-
-* Give the handle and real name of every player from [Croatia]. <-- replace with a country you have in your database.
-
-* List all matches where one team swept the other (i.e., the score was 3 games to 0).
-
-* How much total prize money has been given out since the tournament began?
-
-* How many teams are still active?
-
-* List the real names of players from the defunct teams, without repetition.
-
-* Which team has been the most successful (won the most money)?
-
-* Which team has been the winningest (won the most matches)?
-
-* How much money has player [PlayerName] won at tournaments, and playing for which teams?
-
-* Another query of your choosing. Ask something interesting about your data.
-
+select distinct(realName) from players join members 
+on members.memberName = players.handle join teams on teams.teamId = members.teamId 
+join tournaments on tournaments.winner = teams.teamName where gender = 'female';
